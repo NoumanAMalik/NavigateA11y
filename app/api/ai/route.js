7a848198958e83a6e6e5b706d62689a5170f9cfe
Nouman <noumancodes@gmail.com>
2024-02-18 00:23:37 -0500

    updated gpt model

--- app/api/ai/route.js
+++ app/api/ai/route.js
@@ -23,7 +23,7 @@ export async function POST(request) {
                 content: `This is the HTML of a wbsite: \n ${html} \n\n From this code, give me a few short suggestions  about how this site can be made more accessible. These suggestions  can be about about bad semantics like using too many <div>s or <iframes> (where as good semantics are using tags like <p> or alt texts in for <img>s). Your evaluation of the accecibilty of the HTML should be based on how easy it is to navigate the HTML. Vauge tags would make it less accesible. Give me ONLY 3 suggestions of how the structure of the HTML can be improved. Give me these three suggestions in point form. Each point must only be 2-3 sentences NOT MORE NOT LESS. Keep the suggestions brief.`,
             },
         ],
-        model: "gpt-4",
+        model: "gpt-4-1106-preview",
     });
     console.log(chatCompletion.choices[0].message.content);
 
