b1b721e238324f6b1451bb97cd0783d4bc9fc5c9
Ahmad <ahmadarain2005@gmail.com>
2024-02-17 23:58:25 -0500

    GPT prompt

--- app/api/ai/route.js
+++ app/api/ai/route.js
@@ -17,7 +17,7 @@ export async function POST(request) {
     });
 
     const chatCompletion = await openai.chat.completions.create({
-        messages: [{ role: "user", content: "Say this is a test" }],
+        messages: [{ role: "user", content: `This is the HTML of a wbsite: \n ${html} \n\n From this code, give me a few short suggestions  about how this site can be made more accessible. These suggestions  can be about about bad semantics like using too many <div>s or <iframes> (where as good semantics are using tags like <p> or alt texts in for <img>s). Your evaluation of the accecibilty of the HTML should be based on how easy it is to navigate the HTML. Vauge tags would make it less accesible. Give me ONLY 3 suggestions of how the structure of the HTML can be improved. Give me these three suggestions in point form. Each point must only be 2-3 sentences NOT MORE NOT LESS. Keep the suggestions brief.` }],
         model: "gpt-4",
     });
     console.log(chatCompletion.choices[0].message.content);
