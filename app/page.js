1145bfaa9e5c852c84e07aad1e7ff161eceb1ff0
Nouman <noumancodes@gmail.com>
2024-02-18 02:45:52 -0500

    Styling AI Feedback Modal

--- app/page.js
+++ app/page.js
@@ -323,11 +323,39 @@ export default function Home() {
                         className="modal modal-bottom sm:modal-middle"
                     >
                         <div className="modal-box">
-                            <h3 className="font-bold text-lg">Hello!</h3>
+                            <h3 className="font-bold text-lg">
+                                {aiLoading ? (
+                                    <>
+                                        <p>Feedback Loading</p>{" "}
+                                        {
+                                            <span class="loading loading-dots loading-md"></span>
+                                        }
+                                    </>
+                                ) : (
+                                    <>
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            strokeWidth={1.5}
+                                            stroke="currentColor"
+                                            className="w-6 h-6"
+                                        >
+                                            <path
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z"
+                                            />
+                                        </svg>
+                                        Here are some ideas to make your website
+                                        more accessible :)
+                                    </>
+                                )}
+                            </h3>
                             <p className="py-4">
                                 {/* <>{aiFeedback.text}</> */}
                                 <Markdown remarkPlugins={[remarkGfm]}>
-                                    {aiFeedback.text}
+                                    {aiLoading ? "   " : aiFeedback.text}
                                 </Markdown>
                             </p>
                             <div className="modal-action">
